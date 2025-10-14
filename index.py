28dacf0c574da0a6e84c3d35b1d1a99511e144a9
qingdog <qingdoor@gmail.com>
2025-10-14 17:27:14 +0800

    first commit

---
+++ index.py
@@ -0,0 +1,12 @@
+import xianyu
+#from utils import color_format_logging
+
+
+def main(*_):
+    print("xianyu...")
+    #color_format_logging.main()
+    #ai_api_ephone.main()
+
+
+if __name__ == "__main__":
+    main()
